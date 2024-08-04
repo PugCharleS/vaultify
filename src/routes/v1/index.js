9db0e802816cb81627f6c769434a6a08222dee67
nuggz <cmst010@gmail.com>
2024-08-04 03:02:03 -0600

    Added health check endpoint

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import authRoutes from './authRoutes.js';
 import vaultRoutes from './vaultRoutes.js';
 import passwordRoutes from './passwordRoutes.js';
+import healthRoutes from './healthRoutes.js';
 import { combinedAuth, verifyAppsmithSignature } from '../../middlewares/authMiddleware.js';
 
 const router = Router();
 
+router.use('/health', verifyAppsmithSignature, healthRoutes)
 router.use('/auth', verifyAppsmithSignature, authRoutes);
 router.use('/vaults', combinedAuth, vaultRoutes);
 router.use('/passwords', combinedAuth, passwordRoutes);
